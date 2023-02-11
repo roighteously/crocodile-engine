040637f1d4f252da0ee6c43a8a1f0a8e35ed75b0
Roi <68617740+Roizor@users.noreply.github.com>
2023-02-10 19:46:18 -0600

    remove the scrool barr!!!

--- src/HTMLHelper.ts
+++ src/HTMLHelper.ts
@@ -5,7 +5,7 @@ class htmlhelper {
     public new(elem: string, id: string, classes?: string) {
         let te: any = document.createElement(elem);
         te.id = id;
-        if(elem == "canvas") te.width = document.body.clientWidth; te.height = document.body.clientHeight;
+        if(elem == "canvas") te.width = window.innerWidth - 35; te.height = window.innerHeight - 35;
         if(classes) te.className = classes;
         return te;
     }
