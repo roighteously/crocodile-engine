984d3cc5f93753b5871e57e266011eeffa8784cc
Roi <68617740+Roizor@users.noreply.github.com>
2023-02-10 19:39:59 -0600

    Update HTMLHelper.ts

--- src/HTMLHelper.ts
+++ src/HTMLHelper.ts
@@ -5,7 +5,7 @@ class htmlhelper {
     public new(elem: string, id: string, classes?: string) {
         let te: any = document.createElement(elem);
         te.id = id;
-        if(elem == "canvas") te.width = window.innerWidth; te.height = window.innerHeight;
+        if(elem == "canvas") te.width = document.body.clientWidth; te.height = document.body.clientHeight;
         if(classes) te.className = classes;
         return te;
     }
